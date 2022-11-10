8886ace60959241e6ccc0c906a59e5b87aeac1b8
sksksk705 <sksksk705@hanyang.ac.kr>
2022-11-10 23:57:06 +0900

    [Feature] http 통신 구현, json Decode

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -9,16 +11,51 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getlocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      print(position);
+    } catch (e) {
+      print("error");
+    }
+  }
 
-  void getlocation () async {
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    print(position);
+  void fetchData() async {
+    try {
+      http.Response response = await http.get(Uri.parse(
+          "https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1"));
+      if (response.statusCode == 200) {
+        String jsonData = response.body;
+        var weather = jsonDecode(jsonData)['weather'][0]['description'];
+        print(weather);
+
+        var wind = jsonDecode(jsonData)['weather']['speed'];
+        print(weather);
+
+        var id = jsonDecode(jsonData)['id'];
+        print(weather);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getlocation();
+    fetchData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: ElevatedButton(child: Text("Get My Location"),onPressed: (){getlocation();},)),
+      body: Center(
+          child: ElevatedButton(
+        child: Text("Get My Location"),
+        onPressed: () {},
+      )),
     );
   }
 }
